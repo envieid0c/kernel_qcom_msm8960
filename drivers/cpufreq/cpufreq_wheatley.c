5086603b455652dcf1ccd6fc426aad8fdaa9ddaa
envieidoc <envieidoc@gmail.com>
2019-01-28 10:36:37 +0000

    rename cpufreq_hyper HYPER > hyper

--- drivers/cpufreq/cpufreq_wheatley.c
+++ drivers/cpufreq/cpufreq_wheatley.c
@@ -30,13 +30,13 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(10)
+#define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(20)
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
-#define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(50000)
+#define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(10000)
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
 #define DEF_TARGET_RESIDENCY			(10000)
@@ -57,7 +57,7 @@
 static unsigned int min_sampling_rate, num_misses;
 
 #define LATENCY_MULTIPLIER			(1000)
-#define MIN_LATENCY_MULTIPLIER			(20)
+#define MIN_LATENCY_MULTIPLIER			(100)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
 
 static void do_dbs_timer(struct work_struct *work);
@@ -78,10 +78,10 @@ struct cpufreq_governor cpufreq_gov_wheatley = {
 enum {DBS_NORMAL_SAMPLE, DBS_SUB_SAMPLE};
 
 struct cpu_dbs_info_s {
-    cputime64_t prev_cpu_idle;
-    cputime64_t prev_cpu_iowait;
-    cputime64_t prev_cpu_wall;
-    cputime64_t prev_cpu_nice;
+    u64 prev_cpu_idle;
+    u64 prev_cpu_iowait;
+    u64 prev_cpu_wall;
+    u64 prev_cpu_nice;
     struct cpufreq_policy *cur_policy;
     struct delayed_work work;
     struct cpufreq_frequency_table *freq_table;
@@ -154,19 +154,8 @@ static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu,
     return jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
-{
-    u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
 
-    if (idle_time == -1ULL)
-    return get_cpu_idle_time_jiffy(cpu, wall);
-    else
-    idle_time += get_cpu_iowait_time_us(cpu, wall);
-
-    return idle_time;
-}
-
-static inline cputime64_t get_cpu_iowait_time(unsigned int cpu, cputime64_t *wall)
+static inline u64 get_cpu_iowait_time(unsigned int cpu, u64 *wall)
 {
     u64 iowait_time = get_cpu_iowait_time_us(cpu, wall);
 
@@ -357,7 +346,7 @@ static ssize_t store_ignore_nice_load(struct kobject *a, struct attribute *b,
     struct cpu_dbs_info_s *dbs_info;
     dbs_info = &per_cpu(od_cpu_dbs_info, j);
     dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
-			    &dbs_info->prev_cpu_wall);
+			    &dbs_info->prev_cpu_wall, dbs_tuners_ins.io_is_busy);
     if (dbs_tuners_ins.ignore_nice)
         dbs_info->prev_cpu_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE];
 
@@ -482,7 +471,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
     for_each_cpu(j, policy->cpus) {
     struct cpu_dbs_info_s *j_dbs_info;
-    cputime64_t cur_wall_time, cur_idle_time, cur_iowait_time;
+    u64 cur_wall_time, cur_idle_time, cur_iowait_time;
     unsigned int idle_time, wall_time, iowait_time;
     unsigned int load, load_freq;
     int freq_avg;
@@ -492,7 +481,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
     j_dbs_info = &per_cpu(od_cpu_dbs_info, j);
 
-    cur_idle_time = get_cpu_idle_time(j, &cur_wall_time);
+    cur_idle_time = get_cpu_idle_time(j, &cur_wall_time, dbs_tuners_ins.io_is_busy);
     cur_iowait_time = get_cpu_iowait_time(j, &cur_wall_time);
 
     wall_time = (unsigned int) (cur_wall_time - j_dbs_info->prev_cpu_wall);
@@ -505,7 +494,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
     j_dbs_info->prev_cpu_iowait = cur_iowait_time;
 
     if (dbs_tuners_ins.ignore_nice) {
-        cputime64_t cur_nice;
+        u64 cur_nice;
         unsigned long cur_nice_jiffies;
 
         cur_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE] -
@@ -549,14 +538,12 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 /*
     if (j_cpuidle_dev)
         deepidle_state = &j_cpuidle_dev->states[j_cpuidle_dev->state_count - 1];
-
     if (deepidle_state) {
         deepidle_time = deepidle_state->time;
         deepidle_usage = deepidle_state->usage;
 	    
         total_idletime += (unsigned long)(deepidle_time - j_dbs_info->prev_idletime);
         total_usage += (unsigned long)(deepidle_usage - j_dbs_info->prev_idleusage);
-
         j_dbs_info->prev_idletime = deepidle_time;
         j_dbs_info->prev_idleusage = deepidle_usage;
     }
@@ -722,7 +709,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
         j_dbs_info->cur_policy = policy;
 
         j_dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
-			      &j_dbs_info->prev_cpu_wall);
+			      &j_dbs_info->prev_cpu_wall, dbs_tuners_ins.io_is_busy);
         if (dbs_tuners_ins.ignore_nice) {
 	j_dbs_info->prev_cpu_nice =
 	    kcpustat_cpu(j).cpustat[CPUTIME_NICE];
