0fe3fbcea27eb077e65e28a7d26ff8d287442a39
envieid0c <enveidoc@gmail.com>
2019-02-22 01:58:22 +0000

    Revert "cpufreq: Add Chill cpu gov"
    This reverts commit 143d8ba22de70dbf096cbd10287011cc5a972622.

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -194,10 +194,7 @@ struct cs_dbs_tuners {
     unsigned int sampling_down_factor;
     unsigned int up_threshold;
     unsigned int down_threshold;
-    unsigned int down_threshold_suspended;
     unsigned int freq_step;
-    u64 input_boost_duration;
-    unsigned int sleep_depth;
     unsigned int input_boost_freq;
     unsigned int input_boost_duration;
     unsigned int twostep_threshold;
