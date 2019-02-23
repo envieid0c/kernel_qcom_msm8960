629f474ebc267c338b73930220cd258a18290876
envieid0c <enveidoc@gmail.com>
2019-02-23 02:06:57 +0000

    state_notifier: Check delayed work status before cancel Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/state_notifier.c
+++ arch/arm/mach-msm/state_notifier.c
@@ -1,8 +1,9 @@
 /*
  * State Notifier Driver
  *
- * Copyright (c) 2013-2016, Pranav Vashi <neobuddy89@gmail.com>
+ * Copyright (c) 2013-2017, Pranav Vashi <neobuddy89@gmail.com>
  *           (c) 2017, Joe Maples <joe@frap129.org>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
@@ -13,7 +14,7 @@
 #include <linux/module.h>
 #include <linux/state_notifier.h>
 
-#define DEFAULT_SUSPEND_DEFER_TIME 	10
+#define DEFAULT_SUSPEND_DEFER_TIME 	1
 #define STATE_NOTIFIER			"state_notifier"
 
 /*
@@ -47,7 +48,7 @@ static BLOCKING_NOTIFIER_HEAD(state_notifier_list);
  */
 int state_register_client(struct notifier_block *nb)
 {
-    return blocking_notifier_chain_register(&state_notifier_list, nb);
+	return blocking_notifier_chain_register(&state_notifier_list, nb);
 }
 EXPORT_SYMBOL(state_register_client);
 
@@ -57,7 +58,7 @@ EXPORT_SYMBOL(state_register_client);
  */
 int state_unregister_client(struct notifier_block *nb)
 {
-    return blocking_notifier_chain_unregister(&state_notifier_list, nb);
+	return blocking_notifier_chain_unregister(&state_notifier_list, nb);
 }
 EXPORT_SYMBOL(state_unregister_client);
 
@@ -69,57 +70,61 @@ EXPORT_SYMBOL(state_unregister_client);
  */
 int state_notifier_call_chain(unsigned long val, void *v)
 {
-    return blocking_notifier_call_chain(&state_notifier_list, val, v);
+	return blocking_notifier_call_chain(&state_notifier_list, val, v);
 }
 EXPORT_SYMBOL_GPL(state_notifier_call_chain);
 
 static void _suspend_work(struct work_struct *work)
 {
-    state_suspended = true;
-    state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
-    suspend_in_progress = false;
-    dprintk("%s: suspend completed.\n", STATE_NOTIFIER);
+	state_suspended = true;
+	state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+	suspend_in_progress = false;
+	dprintk("%s: suspend completed.\n", STATE_NOTIFIER);
 }
 
 static void _resume_work(struct work_struct *work)
 {
-    state_suspended = false;
-    state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
-    dprintk("%s: resume completed.\n", STATE_NOTIFIER);
+	state_suspended = false;
+	state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+	dprintk("%s: resume completed.\n", STATE_NOTIFIER);
 }
 
 void state_suspend(void)
 {
-    dprintk("%s: suspend called.\n", STATE_NOTIFIER);
-    if (state_suspended || suspend_in_progress || !enabled)
-	return;
+	dprintk("%s: suspend called.\n", STATE_NOTIFIER);
+	if (state_suspended || suspend_in_progress || !enabled)
+		return;
 
-    suspend_in_progress = true;
+	suspend_in_progress = true;
 
-    queue_delayed_work(susp_wq, &suspend_work,
-	msecs_to_jiffies(suspend_defer_time * 1000));
+	queue_delayed_work(susp_wq, &suspend_work,
+		msecs_to_jiffies(suspend_defer_time * 1000));
 }
 
 void state_resume(void)
 {
-    dprintk("%s: resume called.\n", STATE_NOTIFIER);
-    cancel_delayed_work_sync(&suspend_work);
-    suspend_in_progress = false;
+	dprintk("%s: resume called.\n", STATE_NOTIFIER);
+	if (delayed_work_pending(&suspend_work))
+		cancel_delayed_work_sync(&suspend_work);
+		suspend_in_progress = false;
 
-    if (state_suspended)
-	queue_work(susp_wq, &resume_work);
+	if (state_suspended)
+		queue_work(susp_wq, &resume_work);
 }
 
 static int __init state_notifier_init(void)
 {
-    susp_wq = create_singlethread_workqueue("state_susp_wq");
-    if (!susp_wq)
-	pr_err("State Notifier failed to allocate suspend workqueue\n");
+	susp_wq =
+		alloc_workqueue("state_susp_wq",
+			WQ_HIGHPRI | WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
 
-    INIT_DELAYED_WORK(&suspend_work, _suspend_work);
-    INIT_WORK(&resume_work, _resume_work);
+	if (!susp_wq)
+		pr_err("State Notifier failed to allocate suspend workqueue\n");
 
-    return 0;
+	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
+	INIT_WORK(&resume_work, _resume_work);
+
+	return 0;
 }
 
 subsys_initcall(state_notifier_init);
